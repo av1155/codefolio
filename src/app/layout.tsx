a7bc3fbe3cc99085de9e1f7e4072c6b2c0ecb19b
av1155 <117413846+av1155@users.noreply.github.com>
2024-09-28 06:16:28 -0400

    Commented out import statement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./header";
 import Footer from "./footer";
