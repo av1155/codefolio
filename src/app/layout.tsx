0113111146b96028d001c591cb549d46ba6958b5
av1155 <117413846+av1155@users.noreply.github.com>
2024-09-28 06:14:32 -0400

    Commented out unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,16 +14,16 @@ const inter = Inter({
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
-});
+// const geistSans = localFont({
+//     src: "./fonts/GeistVF.woff",
+//     variable: "--font-geist-sans",
+//     weight: "100 900",
+// });
+// const geistMono = localFont({
+//     src: "./fonts/GeistMonoVF.woff",
+//     variable: "--font-geist-mono",
+//     weight: "100 900",
+// });
 
 export const metadata: Metadata = {
     title: "Andrea Venti - Portfolio",
