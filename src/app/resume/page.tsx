bb7cec7ae05ca7f7a932037f37fddcb0aeed497c
av1155 <117413846+av1155@users.noreply.github.com>
2024-09-30 19:16:08 -0400

    Improved PDF rendering

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -23,7 +23,7 @@ export default function ResumePage() {
                 </div>
 
                 {/* Main Content */}
-                <div className="relative z-10 mx-auto max-w-7xl px-6 lg:px-8 py-24 sm:py-32 text-center text-white">
+                <div className="relative z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-24 sm:py-32 text-center text-white">
                     <h1
                         className="text-4xl font-bold tracking-tight sm:text-5xl"
                         data-aos="fade-down"
@@ -36,13 +36,13 @@ export default function ResumePage() {
                             href="/resume.pdf"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="rounded-md bg-white bg-opacity-80 px-6 py-3 text-sm font-semibold text-indigo-700 hover:bg-opacity-100 transform hover:scale-105 transition duration-300 flex items-center"
+                            className="rounded-md bg-white bg-opacity-80 px-4 py-2 text-md font-semibold text-indigo-700 hover:bg-opacity-100 transform hover:scale-105 transition duration-300 flex items-center justify-center w-36"
                         >
                             <ArrowDownTrayIcon className="h-5 w-5 mr-2" />
                             Download
                         </a>
                         <button
-                            className="rounded-md bg-white bg-opacity-80 px-6 py-3 text-sm font-semibold text-indigo-700 hover:bg-opacity-100 transform hover:scale-105 transition duration-300 flex items-center"
+                            className="rounded-md bg-white bg-opacity-80 px-4 py-2 text-md font-semibold text-indigo-700 hover:bg-opacity-100 transform hover:scale-105 transition duration-300 flex items-center justify-center w-36"
                             onClick={() => {
                                 if (navigator.share) {
                                     navigator
@@ -63,11 +63,11 @@ export default function ResumePage() {
                         </button>
                     </div>
 
-                    <div className="mt-2" data-aos="fade-up">
-                        <div className="transform scale-95 sm:scale-90 transition-transform duration-300">
+                    <div className="mt-8 sm:mt-2" data-aos="fade-up">
+                        <div className="transform scale-100 sm:scale-95 transition-transform duration-300">
                             <iframe
-                                src="/resume.pdf#view=FitH"
-                                className="w-full h-screen rounded-lg border-2 border-white"
+                                src="/resume.pdf#zoom=page-width&view=FitH"
+                                className="w-full h-[80vh] sm:h-screen rounded-lg border-2 border-white"
                                 title="Resume PDF"
                             ></iframe>
                         </div>
